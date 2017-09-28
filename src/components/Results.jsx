de58fa59762f80c7a18295d5103f811271ac9712
bryan <bryan.hermsen@nebraska.gov>
2017-09-28 13:52:37 -0500

    - Add validation - Add menu

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import numeral from 'numeral/min/numeral.min.js'
 
 class Results extends React.Component {
     constructor(props) {
@@ -15,14 +16,14 @@ class Results extends React.Component {
                     </div>
                     <div className="row top_buffer">
                         <div className="col-sm-8"><h4>Yearly post-tax dividend</h4></div>
-                        <div className="col-sm-4 dividend">{this.props.results.pre_tax}</div>
+                        <div className="col-sm-4 dividend"><h4>{this.props.results.div_post}</h4></div>
                     </div>
                     <div className="row top_buffer">
                         <div className="col-sm-12 text-muted">Dividend checks are based on the number of people in a household, not on income or spending.</div>
                     </div>
                     <div className="row top_buffer">
                         <div className="col-sm-8"><h4>Cost of carbon fee</h4></div>
-                        <div className="col-sm-4 cost">{this.props.results.carbon_cost}</div>
+                        <div className="col-sm-4 cost"><h4>{this.props.results.carbon_cost}</h4></div>
                     </div>
                     <div className="row top_buffer bottom_buffer">
                         <div className="col-sm-12 text-muted">Calculated by looking at your location and energy usage.</div>
@@ -34,7 +35,11 @@ class Results extends React.Component {
                     </div>
                     <div className="row top_buffer">
                         <div className="col-sm-12 text-muted summary">
-                            <span className="summary_profit">Awesome! You should end up with some extra money each year.</span>
+                            <span className="summary_profit">
+                              {numeral(this.props.results.net_impact).value() > 0 &&
+                                "Awesome! You should end up with some extra money each year."
+                              }
+                            </span>
                         </div>
                     </div>
                     <div className="row top_buffer">
