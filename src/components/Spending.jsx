f06bde43591bf15da1cf485714e811e8ccc15d83
bryan <bryan.hermsen@nebraska.gov>
2017-12-14 15:55:00 -0600

    Switch gas from weekly to monthly

--- src/components/Spending.jsx
+++ src/components/Spending.jsx
@@ -32,13 +32,13 @@ class Spending extends React.Component {
             <div className="form_title">Almost done. Just a couple more questions about your spending.</div>
 
             <div className="spending">
-              We've taken a guess, approximately ${this.props.initial_elec}/mo on electricity, ${this.props.initial_gas}/week on gasoline,
+              We've taken a guess, approximately ${this.props.initial_elec}/mo on electricity, ${this.props.initial_gas}/mo on gasoline,
               and ${this.props.initial_heat}/mo on heating. If this doesn't align with your spending, you can make adjustments here.
             </div>
 
             <form>
               <div className="form-group">
-                <label htmlFor="elec">Typical Monthly Electricity Bill: $ </label>
+                <label htmlFor="elec">How much is your average monthly electricity bill? $ </label>
                 <input size="3" className="form-control expense_text" id="elec" name="elec" 
                   value={this.props.elec} onChange={(event)=>{this.props.handleSlide('elec', event.target.value)}} />
                 <div className="slider_wrapper">
@@ -48,18 +48,18 @@ class Spending extends React.Component {
               </div>
 
               <div className="form-group">
-                <label htmlFor="gas">Typical Weekly Gasoline Expenditure: $ </label>
+                <label htmlFor="gas">How much is your average monthly gasoline expenditure? $ </label>
                 <input size="3" className="form-control expense_text" id="gas" name="gas" 
                   value={this.props.gas} onChange={(event)=>{this.props.handleSlide('gas', event.target.value)}}/>
                 <div className="slider_wrapper">
-                  <Slider min={0} max={300} step={1} value={this.props.gas}
+                  <Slider min={0} max={900} step={1} value={this.props.gas}
                     onChange={(val) => {this.props.handleSlide('gas', val)}}/>
                 </div>
               </div>
 
               {this.props.heat > 0 && (
                 <div className="form-group">
-                  <label htmlFor="heat">Typical Monthly {this.props.heating_type} Expediture: $ </label>
+                  <label htmlFor="heat">How much is your typical monthly {this.props.heating_type} expediture? $ </label>
                   <input size="3" className="form-control expense_text" id="heat" name="heat"
                     value={this.props.heat} onChange={(event)=>{this.props.handleSlide('heat', event.target.value)}} />
                   <div className="slider_wrapper">
