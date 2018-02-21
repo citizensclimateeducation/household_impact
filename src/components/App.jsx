eaf8ba871b021b2e363d787fbbccc710332c8822
bryan <bryan.hermsen@nebraska.gov>
2018-02-21 07:45:50 -0600

    Keep utility slider values between API queries. This will reset the value if it's either zero or if the value is outside of the upper range for that utility for the new query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ const impact_study_url = 'https://ummel.ocpu.io/exampleR/R/predictModel/json'
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { div_pre: 0, mrate: 0.15, elec: 100, gas: 75, heat: 50, cost: '', net_impact: 0, moe: 0, gas_upr: 200, 
+    this.state = { div_pre: 0, mrate: 0.15, elec: 0, gas: 0, heat: 0, cost: '', net_impact: 0, moe: 0, gas_upr: 200, 
       elec_upr: 200, show_footer_impact: false, heat_upr: 300, carbon_cost: 0, div_post: 0, initial_heat: 0, 
       initial_gas: 0, initial_elec: 0, div_month: 0, cost_month: 0, impact_month: 0, adults: 1, children: 0, loading: false, 
       heating_type: 'Natural gas', vehicles: 2, zip: '', dwelling_type: 'Stand-alone house'}
@@ -33,10 +33,17 @@ class App extends React.Component {
   }
 
   setResults = (e) => {
-    this.setState(e)
+    const utilities = ['gas', 'heat', 'elec'];
+    Object.keys(e).forEach((key) => { utilities.includes(key) ? this.updateUtility(key, e[key]) : this.setState({ [key]: e[key] }) })
     this.setState({initial_gas: e.gas, initial_elec: e.elec, initial_heat: e.heat})
     this.calculateCost()
     this.setLoading(false);
+  }
+
+  updateUtility = (key, updatedValue) => {
+    if (updatedValue === 0 || this.state[key] === 0 || this.state[key] > this.state[key + '_upr']) {
+      this.setState({ [key]: updatedValue })
+    }
   }
 
   calculate = (e) => {
@@ -67,10 +74,9 @@ class App extends React.Component {
 
       axios.post(impact_study_url, JSON.stringify(data), {responseType: 'json', headers: {'Content-Type': 'application/json'}}).
         then(function(response) {
+          setResults({...response.data[0]})
           $('.calculating').fadeOut('slow', function() {
-            $('.spending_panel, .btn_results, .calculate_success').fadeIn('slow', function() {
-              setResults({...response.data[0]})
-            });
+            $('.spending_panel, .btn_results, .calculate_success').fadeIn('slow')
           });
         }).catch(function(error) {
           nextSection('#home_questions'); $('.search_failed').fadeIn('slow');
