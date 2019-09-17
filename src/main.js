f5c9c70e11aad429a4a6fdf02e26908bb5b15828
Bryan Hermsen <b_hermse@hotmail.com>
2019-09-17 13:16:27 -0500

    Format updates - use wordpress image for blue check - start adding eicda style overrides

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap-modal'
+import 'bootstrap-modal';
 require('./styles/calculator.scss');
 require('./styles/rangeslider.scss');
 require('./styles/print.scss');
 import $ from 'jquery';
 
-const calculator = document.querySelector('#calculator')
-ReactDOM.render(<App />, calculator)
+const calculator = document.querySelector('#calculator');
+ReactDOM.render(<App />, calculator);
